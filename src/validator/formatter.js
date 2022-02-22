7dd25e715e3c30b2bd19b6acb88221425d430f2b
riteshece <riteshgupta.14august@gmail.com>
2022-02-22 17:50:02 +0530

    this is my nodejs assignment

---
+++ src/validator/formatter.js
@@ -0,0 +1,5 @@
+const module3 = "    Functionup     "
+const newmodule = module3.trim().toLocaleLowerCase()
+const newmoduleee = module3.trim().toLocaleUpperCase()
+console.log(newmodule)
+console.log(newmoduleee)
