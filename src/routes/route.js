603e6f13d892ce72d605dcc56c2b1f723f16442d
riteshece <riteshgupta.14august@gmail.com>
2022-02-24 17:02:48 +0530

    task done

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,73 +1,97 @@
-const express = require('express');
-const router = express.Router();
+const express =require('express');
+const router =express.Router();
+let players = [ 
+    {
+          
+         "name": "manish",
+         "dob": "1/1/1995",
+         "gender":"male",
+         "city": "jalandhar",
+         "sports": [ " swimming"],
+         "bookings": [
 
-router.get('/students/:name', function(req, res) {
-    let studentName = req.params.name
-    console.log(studentName)
-    res.send(studentName)
+            {
+                "bookingNumber":1,
+                "sportsId":"",
+                "centreId":"",
+                "type":"private",
+                "slot": "12345",
+                "bookedOn":"31/08/2021",
+                "bookedFor":" 01/09/2021"
+            },
+            {
+                "bookingNumber":2,
+                "sportsId":"",
+                "centreId":"",
+                "type":"private",
+                "slot": "123456",
+                "bookedOn":"31/08/2021",
+                "bookedFor":" 03/09/2021"
+            }
+
+         ]
+  },
+ 
+ 
+  {
+    "name": "Ranjit",
+    "dob": "1/3/1998",
+    "gender":"male",
+    "city": "jalandhar",
+    "sports": [ " swimming"],
+    "bookings": [   ]
+
+ },
+
+  {
+
+    "name": "mohan",
+    "dob": "1/2/1999",
+    "gender":"male",
+    "city": "jalandhar",
+    "sports": [ " swimming"],
+    "bookings": [  ]
+
+
+
+
+  }
+
+]
+
+let a = players.length;
+
+// Part 1 ==> Add new player.
+
+router.post('/player',function(req,res){
+
+let ele = req.body.nplayer.name;
+let ele2 = req.body.nplayer
+for (let i=0;i<a;i++){
+if(ele === players[i].name ){
+    console.log(ele)
+    res.send("player already exists")
+    
+   
+}
+else if (i === a-1){
+
+    players.push(ele2)
+    console.log(ele2)
+    res.send({data :players , status : true})
+   
+}
+
+}
+
+
+
+
+
 })
 
-router.get("/random" , function(req, res) {
-    res.send("hi there")
-})
-
-
-router.get("/test-api" , function(req, res) {
-    res.send("hi FunctionUp")
-})
-
-
-router.get("/test-api-2" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API")
-})
-
-
-router.get("/test-api-3" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's ")
-})
-
-
-router.get("/test-api-4" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
 
 
 
-router.get("/test-api-5" , function(req, res) {
-    res.send("hi FunctionUp5. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
-
-router.get("/test-api-6" , function(req, res) {
-    res.send({a:56, b: 45})
-})
-
-router.post("/test-post", function(req, res) {
-    res.send([ 23, 45 , 6])
-})
-
-
-router.post("/test-post-2", function(req, res) {
-    res.send(  { msg: "hi" , status: true }  )
-})
-
-router.post("/test-post-3", function(req, res) {
-    // let id = req.body.user
-    // let pwd= req.body.password
-
-    // console.log( id , pwd)
-
-    console.log( req.body )
-
-    res.send(  { msg: "hi" , status: true }  )
-})
-
-
-
-router.post("/test-post-4", function(req, res) {
-    let arr= [ 12, "functionup"]
-    let ele= req.body.element
-    arr.push(ele)
-    res.send(  { msg: arr , status: true }  )
-})
 
 module.exports = router;
