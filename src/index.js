8797444065fa66bce6dcc143cbb3583a6b50a016
riteshece <riteshgupta.14august@gmail.com>
2022-03-05 19:49:48 +0530

    done task

--- src/index.js
+++ src/index.js
@@ -8,11 +8,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://prakash:prakash@cluster0.4qxv0.mongodb.net/prakash?authSource=admin&replicaSet=atlas-13sjqg-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
+
+
+const GlobalMW= function (req, res, next){
+    const Time=  new Date().toISOString().replace(/T/,' ').replace(/\..+/,'') 
+    const Request=  req.originalUrl
+    const IP= req.ip
+    console.log (Time, Request, IP)
+}
+app.use(GlobalMW)
 
 app.use('/', route);
 
